bb23ff08923c4b510df0a58d6d990904824b12ba
Namelessfury <69160890+Namelessfury@users.noreply.github.com>
2022-08-10 00:56:45 -0400

    Working + Checkbox
    Backend delete table button now works. 2 button confirm to delete table

--- admin/tmpl/home/default.php
+++ admin/tmpl/home/default.php
@@ -13,42 +13,34 @@ defined('_JEXEC') or die('Restricted Access');
 
 use Joomla\CMS\Factory;
 
+//deletes all attendance records when the red button is pressed 
+if(isset($_POST['deleteAttendanceRecords']))
+{
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query = "DELETE FROM #__attendance_reports";
+    $db->setQuery($query)->execute();
+    echo 'The attendance records have been deleted';
+}
 ?>
 
 <html>
     <body>
-
+        <br><br>
         <h2>Delete Attendance Records</h2>
 
-        <p>This button will delete ALL current attendance records in the system.</p>
+        <p>This button will <b>delete all attendance records</b> currently in the system</p>
         <p>BE VERY CAREFUL!</p>
+        
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="checkboxConfirm" onchange="document.getElementById('deleteAttendanceRecords').disabled = !this.checked;">
+            <label class="form-check-label" for="checkboxConfirm">Yes, I want to delete all attendance records</label>
+        </div>
 
-        <script>
-            function deleteAttendanceRecords() {
-            let text = "Are you sure you want to delete all the attendance records?\nEither OK or Cancel.";
-            
-            if (confirm(text) == true)
-            {
-                <?php
-                    $db = JFactory::getDbo();
-                    $query = $db->getQuery(true);
-                    $query = "DELETE FROM #__attendance_reports";
-                    $db->setQuery($query)->execute();
-                ?>
+        <br>
 
-                text = "The attendance records have been deleted.";
-            }
-            else
-            {
-                text = "You canceled.";
-            }
-            document.getElementById("response").innerHTML = text;
-            }
-        </script>
-
-        <button class="btn btn-danger" onclick="deleteAttendanceRecords()">Delete All Attendance Records</button>
-
-        <p id="response"></p>
-
+        <form method="post">
+            <input class="btn btn-danger" type="submit" name="deleteAttendanceRecords" id="deleteAttendanceRecords" value="Delete All Attendance Records" disabled/>
+        </form>
     </body>
 </html>
